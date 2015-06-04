9adc0b56710a8c60bc22599012d915747e8fcd5b
Jason Wright <noobjey@gmail.com>
2015-06-04 07:59:53 -0600

    key generator for random keys

---
+++ lib/key_generator.rb
@@ -0,0 +1,10 @@
+class KeyGenerator
+
+  def key
+      Random.rand(99999)
+  end
+
+  def default
+    41521
+  end
+end
---
+++ test/key_generator_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/key_generator'
+
+class KeyGeneratorTest < Minitest::Test
+  def test_key_defaults_to_41521
+    key_generator = KeyGenerator.new
+    expected  = 41521
+
+    result = key_generator.default
+
+    assert_equal expected, result
+  end
+
+  def test_key_generates_five_numbers
+    key_generator = KeyGenerator.new
+    expected  = 5
+
+    result = key_generator.key
+ puts key_generator.key
+    assert_equal expected, result.to_s.length
+  end
+end
